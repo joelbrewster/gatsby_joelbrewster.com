335c714922fe80e21eba44377fc86cc580d886a5
Joel Brewster <hi@joelbrewster.com>
2019-03-29 11:24:06 +1100

    Updates

--- .cache/async-requires.js
+++
@@ -1,9 +0,0 @@
-// prefer default export if available
-const preferDefault = m => m && m.default || m
-
-exports.components = {
-  "component---src-pages-index-js": () => import("/Users/joel/Developer/gatsby/src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */)
-}
-
-exports.data = () => import(/* webpackChunkName: "pages-manifest" */ "/Users/joel/Developer/gatsby/.cache/data.json")
-
