1b9d63001611f733d8620c129232ca23565b51b9
Joel Brewster <hi@joelbrewster.com>
2019-09-20 11:16:23 +1000

    plz

--- src/components/intro.js
+++ src/components/intro.js
@@ -4,10 +4,8 @@ import React from 'react';
 export default props => (
 <section>
         <h1>{props.headerText}</h1>
-        <p>{props.introTop}</p>
-        <p>{props.introMiddle}</p>
-        <p>
-            {props.introBottom}
-        </p>
+        {/* <p>{props.introTop}</p> */}
+        {/* <p>{props.introMiddle}</p> */}
+        {/* <p>{props.introBottom}</p> */}
 </section>
 )
